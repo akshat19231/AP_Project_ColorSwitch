0b5fb0c1b561425cf7651b99e2c990f225836d2d
akshat19231 <63906112+akshat19231@users.noreply.github.com>
2020-11-21 23:40:00 +0530

    3rd-patch1

---
+++ Controller.java
@@ -0,0 +1,75 @@
+package sample;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+import java.io.IOException;
+public class Controller {
+    @FXML
+    private ImageView sRing1;
+    @FXML
+    private ImageView logo2;
+    @FXML
+    private ImageView logo3;
+    @FXML
+    private ImageView sRing2;
+    @FXML
+    private Button exitB;
+    @FXML
+    private Button newGameB;
+    @FXML
+    private Button leaderBoardB;
+    @FXML
+    private Button loadGameB;
+    DropShadow shadow = new DropShadow();
+    private static final String IDLE_BUTTON_STYLE1 = "-fx-background-color: purple;";
+    private static final String IDLE_BUTTON_STYLE2 = "-fx-background-color: white;";
+    private static final String HOVERED_BUTTON_STYLE = "-fx-background-color: -fx-shadow-highlight-color, -fx-outer-border, -fx-inner-border, -fx-body-color;";
+    public void rot(ImageView imv, int mul){
+        RotateTransition rotate = new RotateTransition(Duration.millis(3000));
+        rotate.setAxis(Rotate.Z_AXIS);
+        rotate.setByAngle(360*mul);
+        rotate.setCycleCount(Animation.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+        //rotate.setAutoReverse(true);
+        rotate.setNode(imv);
+        rotate.play();
+    }
+    public void rotate() throws IOException {
+        rot(sRing1,1);
+        rot(sRing2,-1);
+        rot(logo2,1);
+        rot(logo3,-1);
+    }
+    public void highlightOn_e() throws IOException {
+        exitB.setStyle("-fx-background-radius: 100px; -fx-background-color: bda0e0 ;");
+    }
+    public void highlightOff_e() throws IOException {
+        exitB.setStyle("-fx-background-radius: 100px; -fx-background-color: purple;");
+    }
+    public void highlightOn_l() throws IOException {
+        leaderBoardB.setStyle("-fx-background-radius: 100px; -fx-background-color: bda0e0 ;");
+    }
+    public void highlightOff_l() throws IOException {
+        leaderBoardB.setStyle("-fx-background-radius: 100px; -fx-background-color: purple;");
+    }
+    public void highlightOn_n() throws IOException {
+        newGameB.setStyle("-fx-background-radius: 100px; -fx-border-width: 2px; -fx-background-color: a0a3e0 ;");
+    }
+    public void highlightOff_n() throws IOException {
+        newGameB.setStyle("-fx-background-radius: 100px; -fx-border-width: 2px; -fx-background-color: white;");
+    }
+    public void highlightOn_load() throws IOException {
+        loadGameB.setStyle("-fx-background-radius: 100px; -fx-background-color: bda0e0;");
+    }
+    public void highlightOff_load() throws IOException {
+        loadGameB.setStyle("-fx-background-radius: 100px; -fx-background-color: purple;");
+    }
+}
