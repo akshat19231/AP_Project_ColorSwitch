b02ab4d635f393ac2a1c394526a23f99347389a4
Akshat Wadhwa <akshat29802@gmail.com>
2020-11-24 21:56:26 +0530

    easy dnc

--- src/sample/CircleObs.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class CircleObs extends Obstacles {
-    public CircleObs(Double x, Double y, int a, int b, int c, int d, int type) {
-        super(x, y, a, b, c, d, type);
-    }
-}
--- src/sample/stars.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class stars extends gameElements{
-    public stars(Double x, Double y) {
-        super(x, y);
-    }
-}
